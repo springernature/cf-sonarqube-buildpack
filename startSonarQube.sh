fe7f3d91a7b2367fd621a048da8404cd813f6e6d
Joscha Alisch <joscha.alisch@gmail.com>
2018-05-15 15:01:30 +0200

    Replace environment variables in sonar.properties
    Co-authored-by: Thomas Heithausen <thomas.heithausen@springer.com>

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -7,6 +7,11 @@ echo "-----> Setting sonar.properties"
 echo "       sonar.web.port=${PORT}"
 echo "sonar.web.port=${PORT}" >> ./sonar.properties
 
+# Replace all environment variables with syntax ${MY_ENV_VAR} with the value
+# thanks to https://stackoverflow.com/questions/5274343/replacing-environment-variables-in-a-properties-file
+perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./sonar.properties
+
+
 echo "------------------------------------------------------" > /home/vcap/app/sonarqube/logs/sonar.log
 
 echo "-----> Starting SonarQube"
