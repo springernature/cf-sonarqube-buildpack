9a6a16a3fd6505903a1c52db5ca713166585e503
Joscha Alisch <joscha.alisch@gmail.com>
2018-05-15 15:59:44 +0200

    Make sure there is a newline at the end of the user-provided properties

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -5,7 +5,8 @@ export PATH=$PATH:/home/vcap/app/.java/bin
 
 echo "-----> Setting sonar.properties"
 echo "       sonar.web.port=${PORT}"
-echo "sonar.web.port=${PORT}" >> ./sonar.properties
+echo "\n ------- The following properties were automatically created by the buildpack -----\n" >> ./sonar.properties
+echo "sonar.web.port=${PORT}\n" >> ./sonar.properties
 
 # Replace all environment variables with syntax ${MY_ENV_VAR} with the value
 # thanks to https://stackoverflow.com/questions/5274343/replacing-environment-variables-in-a-properties-file
