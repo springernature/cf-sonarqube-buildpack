9e0deb02500193e7780536cb0bdef167f968a0ad
Lukas Müller <lukas.mueller@springernature.com>
2018-10-11 15:25:09 +0200

    Update distribution url
    Co-authored-by: Isabel Batista <isabel.schaeferbatista@springernature.com>

--- vars.sh
+++ vars.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export JRE_URL=http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jre-8u181-linux-x64.tar.gz
-export SONARQUBE_DIST_URL="https://sonarsource.bintray.com/Distribution"
+export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
+
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
