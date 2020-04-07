d24ce924c3273ddc51816cf1a8ab176f58c0f33b
Steve Brandt <steve.brandt@springer.com>
2020-04-07 08:49:00 +0200

    use of OpenJDK11 to fulfill prerequisite for sonarqube 8

--- vars.sh
+++ vars.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
+#export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
+export JRE_URL=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk11u-2020-04-07-04-21/OpenJDK11U-jdk_x64_linux_hotspot_2020-04-07-04-21.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
