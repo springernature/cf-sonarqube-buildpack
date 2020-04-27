5bb6088f96d792fe27c5e3dc5a4beb4a7bca7075
Steve Brandt <steve.brandt@springer.com>
2020-04-27 16:22:53 +0200

    try to get it production ready by setting vm max map count

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -18,7 +18,10 @@ echo "------------------------------------------------------" > /home/vcap/app/s
 
 echo "-----> set vm max map count"
 
-sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.max_map_count=262144
+sudo sysctl -w fs.file-max=65536
+ulimit -n 65536
+ulimit -u 4096
 
 echo "-----> Starting SonarQube"
 
