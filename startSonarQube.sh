a586da17721a0ddb51f8f204cd6d55de7a30c875
Steve Brandt <steve.brandt@springer.com>
2020-04-27 20:32:07 +0200

    try to get it production ready by setting vm max map count

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -20,8 +20,8 @@ echo "------------------------------------------------------" > /home/vcap/app/s
 echo "-----> set vm max map count"
 #echo "vm.max_map_count=262144" >> /etc/sysctl.d/99-sysctl.conf
 #echo "fs.file-max=65536" >> /etc/sysctl.d/99-sysctl.conf
-sudo sysctl vm.max_map_count
-sudo sysctl fs.file-max
+#sudo sysctl vm.max_map_count
+#sudo sysctl fs.file-max
 
 #echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 #echo "fs.file-max=65536" >> /etc/sysctl.conf
@@ -39,6 +39,13 @@ echo "-----> Tailing log"
 sleep 10 # give it a bit of time to create files
 cd /home/vcap/app/sonarqube/logs
 tail -f ./sonar.log ./es.log ./web.log ./ce.log ./access.log
+
+echo "-----> set vm max map count after"
+sysctl vm.max_map_coun
+sysctl fs.file-max
+
+sudo sysctl vm.max_map_count
+sudo sysctl fs.file-max
 
 #echo "-----> set discovery.type: single-node and restart"
 #echo "discovery.type: single-node" >> /home/vcap/app/sonarqube/temp/conf/es/elasticsearch.yml
